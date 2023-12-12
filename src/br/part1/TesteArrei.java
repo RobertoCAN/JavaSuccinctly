e53f8f85144e0675718f9c5af15bd8c28a98b786
Roberto Caetano Assunção Neto <robertofreeware@gmail.com>
2023-12-12 16:57:11 -0300

    Projeto SOLID 06

---
+++ src/br/part1/TesteArrei.java
@@ -0,0 +1,15 @@
+package br.part1;
+
+public class TesteArrei {
+
+	public static void main(String[] args) {
+		Arrei inicio = new Arrei();
+		
+		inicio.number1 = 5;
+		inicio.number2 = 7;
+		
+		System.out.println(inicio.array2d);
+
+	}
+
+}
