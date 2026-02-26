962c9363a5574ac4e8dd99bfb748e02b32c52c8d
RobertoCAN <roberto.caetano@cegas.com.br>
2026-02-26 15:14:55 -0300

    Using an anonymous class as a parameter

---
+++ src/sussinclyPart2/p43/AnonymousP45.java
@@ -0,0 +1,13 @@
+package sussinclyPart2.p43;
+
+public class AnonymousP45 {
+	
+	public int operation(int a, int b) {
+		return 0;
+		
+	}
+	
+	static int performOperation(int a, int b, AnonymousP45 op) {
+		return op.operation(a, b);
+	}
+}
